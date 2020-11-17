6ddebd0c97b1b0bf322c96e16d826d558c3c93f8
Jason Zhou <jzhou35@buffalo.edu>
2020-11-17 18:43:04 -0500

    commit tree 7a5783c6b995423062e2475eb7f1c11105241003

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name,roster = {})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(student_name,student_grade)
+        @student_name = student_name
+        @student_grade = student_grade
+        if @roster.has_key?(@student_grade)
+            @roster[@student_grade] << @student_name
+        else
+            @roster[@student_grade] = []
+            @roster[@student_grade] << @student_name
+        end
+    end
+
+    def roster
+        @roster
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        @roster.each do |key,value|
+            @roster[key] = value.sort
+        end
+    end
+end
